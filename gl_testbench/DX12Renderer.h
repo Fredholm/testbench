09b12ce20b87f863b18203030b0fa9745a1dd541
Fredholm <te11c.simon.fredholm@gmail.com>
2018-01-17 22:26:55 +0100

    Initial DirectX Objects

--- gl_testbench/DX12Renderer.h
+++ gl_testbench/DX12Renderer.h
@@ -5,6 +5,10 @@
 
 // DirectX12 Includes 
 #include <d3d12.h>
+#include <d3dx12.h> // DX12 helper functions and structs
+#include <dxgi1_4.h>
+#include <D3Dcompiler.h>
+#include <DirectXMath.h>
 
 // SDL Includes and Libs
 #include <SDL.h>
@@ -13,6 +17,9 @@
 
 class DX12Renderer : public Renderer
 {
+private:
+    static const int AmountOfFrames = 2;
+
 public:
     DX12Renderer();
     ~DX12Renderer();
@@ -39,20 +46,52 @@ public:
     // can be partially overriden by a specific Technique.
     void setRenderState(RenderState* ps);
 
-    // submit work (to render) to the renderer.
+    // \brief Submitting work to the renderer
     void submit(Mesh* mesh);
+
+    // \brief Called each frame 
+    // Basicly the render function
     void frame();
 
 private:
 
+    ///////////////////////////////////
     // SDL Window Creation and objects
     void initSDLWindown(unsigned int width, unsigned int height);
-    SDL_Window* window;
-    SDL_GLContext context;
+    SDL_Window*                 window;
+    SDL_GLContext               context;
+    ///////////////////////////////////
 
-    // DX12 Pipeline
+    CD3DX12_VIEWPORT            m_Viewport;
+    CD3DX12_RECT                m_ScissorRect;
 
-    // DX12 Objects
+    // DirectX12 Pipeline
+    IDXGISwapChain3*            m_SwapChain;                        //< IDXGISwapChain3::GetCurrentBackBufferIndex to choose which Render Target View to render to each frame
+    ID3D12Device*               m_Device;                           //< Used for a bunch of things, there's no DeviceContext anymore either, Ex Func: CreateCommited/Reserved Resource
+    ID3D12Resource*             m_RenderTargets[AmountOfFrames];    //< Contains arrays of data, Enables both the GPU and CPU to read the physical memory   
+                                                                    //< \note CreateReservedResource is just virtual, while CreateCommitedResource is both physical and virtual, CreateHeap is still just physical
+    // DirectX Command Things 
+    // (Work submission was automaticly in DirectX11, 
+    //  so this is all new stuff)
+    ID3D12CommandAllocator*     m_CommandAllocator;                 //< Used to back memory for the commands
+    ID3D12CommandQueue*         m_CommandQueue;                     //< FIFO scheduler
+    ID3D12CommandList*          m_CommandList;                      //< (Similar to deffered context in D11), Records commands to be used later
+    ID3D12RootSignature*        m_RootSignature;                    //< Used to Link command lists to the resources the shaders need
+
+    // DirectX Other Things
+    ID3D12DescriptorHeap*       m_DescriptorHeap;                   //< Used for Resource Binding, They completly reworked the RB system, -> CommandList::Set*Root()DescriptorTable blabla
+    ID3D12PipelineState*        m_PipelineState;                    //< Used to identify and use different stuff (VS, PS, HS, DS, GS, CS, OM, RS, IA)
+    ID3D12GraphicsCommandList*  m_GraphicsCommandList;              //< Copied from docs, since I've never used it: Includes APIs for instrumenting the command list execution, and for setting and clearing the pipeline state
+    
+    // DirectX Triangles Things
+    ID3D12Resource*             m_VertexBuffer;
+    D3D12_VERTEX_BUFFER_VIEW    m_VertexBufferView;
+
+    // DirectX Fence Sync
+    ID3D12Fence*                m_Fence;                            //< Used to jump between and sync GPU and CPU
+    UINT                        m_FrameIndex;                       //< Current Fence Index
+    HANDLE                      m_FenceEvent;                       //< Current Fence Event
+    UINT64                      m_FenceValue;                       //< Current Fence Value
 };
 
 
