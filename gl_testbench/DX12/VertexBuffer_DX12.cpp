7d65e86ed9fd2da51e6133b8cb57882255d812c1
Fredholm <te11c.simon.fredholm@gmail.com>
2018-01-19 02:45:26 +0100

    Prep for tommorow, Stable

--- gl_testbench/DX12/VertexBuffer_DX12.cpp
+++ gl_testbench/DX12/VertexBuffer_DX12.cpp
@@ -1,19 +1,24 @@
 #include "VertexBuffer_DX12.h"
 
-VertexBuffer_DX12::VertexBuffer_DX12() { }
+VertexBuffer_DX12::VertexBuffer_DX12(size_t size, VertexBuffer::DATA_USAGE usage, ID3D12Device3* device)
+{
+}
 
 VertexBuffer_DX12::~VertexBuffer_DX12() { }
 
 void VertexBuffer_DX12::setData(const void * data, size_t size, size_t offset)
 {
+
 }
 
 void VertexBuffer_DX12::bind(size_t offset, size_t size, unsigned int location)
 {
+
 }
 
 void VertexBuffer_DX12::unbind()
 {
+
 }
 
 size_t VertexBuffer_DX12::getSize()
--- gl_testbench/DX12/VertexBuffer_DX12.h
+++ gl_testbench/DX12/VertexBuffer_DX12.h
@@ -1,8 +1,16 @@
 #ifndef VERTEXBUFFER_DX12_H
 #define VERTEXBUFFER_DX12_H
 
+#include <Windows.h>
+#include <d3d12.h>
+#include <d3dx12.h> // DX12 helper functions and structs
+#include <dxgi1_4.h>
 #include <DirectXMath.h>
 #include "../VertexBuffer.h"
+#include "Utility.h"
+
+#pragma comment(lib,"D3D12.lib")
+#pragma comment(lib,"DXGI.lib")
 
 #define _USE_MATH_DEFINES
 
@@ -17,14 +25,16 @@ struct Vertex
 class VertexBuffer_DX12 : public VertexBuffer
 {
 public:
-
-public:
-    VertexBuffer_DX12();
+    VertexBuffer_DX12(size_t size, VertexBuffer::DATA_USAGE usage, ID3D12Device3* device);
     ~VertexBuffer_DX12();
     void setData(const void* data, size_t size, size_t offset);
     void bind(size_t offset, size_t size, unsigned int location);
     void unbind();
     size_t getSize();
+
+private:
+    ID3D12Resource * m_VertexBuffer;
+    D3D12_VERTEX_BUFFER_VIEW m_VertexBufferView;
 };
 
 #endif // !VERTEXBUFFER_DX12_H
