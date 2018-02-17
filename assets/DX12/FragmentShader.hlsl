6878c1c3b85e9105e2644df5737bbbd51e55c3ea
Henrik Vik <henkvik@gmail.com>
2018-02-17 13:21:12 +0100

    added ifdef to shader

--- assets/DX12/FragmentShader.hlsl
+++ assets/DX12/FragmentShader.hlsl
@@ -6,14 +6,16 @@ cbuffer SceneDiffuseColor : register(B(DIFFUSE_TINT))
 	float4 diffuse;
 };
 
+#ifdef DIFFUSE_SLOT
 Texture2D g_texture : register(T(DIFFUSE_SLOT));
+#endif
 SamplerState g_sampler : register(s0);
 
 struct PSInput
 {
 	float4 position : SV_POSITION;
-    float4 normal : _NORMAL;
-    float2 tex : _TEXTCOORD;
+	float4 normal : _NORMAL;
+	float2 tex : _TEXTCOORD;
 };
 
 float4 PSMain(PSInput input) : SV_TARGET
@@ -21,7 +23,7 @@ float4 PSMain(PSInput input) : SV_TARGET
 #ifdef DIFFUSE_SLOT
 	float4 col = g_texture.Sample(g_sampler, input.tex);
 #else
-	float4 col = float4(1.0,1.0,0.0, 1.0);
+	float4 col = float4(1.0,1.0,1.0, 1.0);
 #endif
 
 	return col * diffuse;
